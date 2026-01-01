de9a54d2201e84294fb2e7bf278d31fd98807006
Aman Kumar Gupta <agupta11040@gmail.com>
2026-01-01 12:03:13 +0530

    Time: 1 ms (35.05%) | Memory: 46.5 MB (31.20%) - LeetSync

---
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
@@ -0,0 +1,36 @@
+class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode prevGroup = dummy;
+
+        while (true) {
+            // Check if k nodes exist
+            ListNode kth = prevGroup;
+            for (int i = 0; i < k && kth != null; i++) {
+                kth = kth.next;
+            }
+            if (kth == null) break;
+
+            ListNode groupNext = kth.next;
+            ListNode prev = groupNext;
+            ListNode curr = prevGroup.next;
+
+            // Reverse k nodes
+            while (curr != groupNext) {
+                ListNode temp = curr.next;
+                curr.next = prev;
+                prev = curr;
+                curr = temp;
+            }
+
+            // Connect reversed group
+            ListNode temp = prevGroup.next;
+            prevGroup.next = kth;
+            prevGroup = temp;
+        }
+
+        return dummy.next;
+    }
+}
